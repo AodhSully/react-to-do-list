855609e5b34d67388ff0fa5b468c355e382f2abe
AodhSully <aodhsully@gmail.com>
2019-08-15 18:08:04 +0100

    to do list made with react & react router

---
+++ src/components/ToDos.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ToDoItem from './ToDoItem'
+import PropTypes from 'prop-types'
+
+class ToDos extends Component {
+
+
+  render() {
+    return this.props.todos.map((todo) => (
+      <ToDoItem key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        delTodo={this.props.delTodo}
+        />
+    ));
+  }
+}
+
+//proptypes
+ToDos.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+
+}
+
+export default ToDos;
